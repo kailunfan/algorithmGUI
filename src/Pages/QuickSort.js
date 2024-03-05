97b027d05b2d0bbfc55bf40694805c6d8df28c6d
fankailun <fankailun@58.com>
2024-03-05 16:59:46 +0800

    feat: new quick sort

--- src/Pages/QuickSort.js
+++ src/Pages/QuickSort.js
@@ -1,79 +1,146 @@
-import React, { Fragment } from "react";
-import { Base } from './Base';
+import React from "react";
+import { Button } from "antd";
+import { CheckCircleOutlined } from "@ant-design/icons";
 
-export class QuickSort extends Base {
+export const Item = ({ children, toCompare, toSwap, sorted }) => {
+  let color = "inherit";
+  if (toCompare) {
+    color = "#67C23A";
+  }
+  if (toSwap) {
+    color = "#F56C6C";
+  }
+  const background = sorted ? "#C0C4CC" : "inherit";
+
+  return (
+    <div className="square" style={{ float: "none", color, background }}>
+      {children}
+    </div>
+  );
+};
+
+export class QuickSort extends React.Component {
   constructor(props) {
     super(props);
-    this.list = [5, 4, 3, 1, 9, 8, 0, 6, 2, 7];
-    this.state = {
-      list: this.list,
-      cur: [-1, -1],
-      pivotIndex: -1,
-      lc: -1,
-      rc: -1
+    this.initialState = {
+      list: [5, 4, 3, 1, 9, 8, 0, 6, 2, 7],
+      i: -1,
+      j: -1,
+      s: -1,
+      toSwapIndex: [],
+      toCompareIndex: [],
+      done: false,
     };
+    this.state = JSON.parse(JSON.stringify(this.initialState));
+    this.process = this.start();
   }
 
-  async quickSort(list, startIndex, endIndex) {
-    if (startIndex === undefined) {
-      startIndex = 0;
-    }
-    if (endIndex === undefined) {
-      endIndex = list.length - 1;
-    }
+  *start() {
+    yield* this.quickSort(this.state.list, 0, this.state.list.length - 1);
+    // done
+    this.setState({
+      done: true,
+      i: -1,
+      j: -1,
+      s: -1,
+      toSwapIndex: [],
+      toCompareIndex: [],
+    });
+  }
+
+  *quickSort(list, startIndex, endIndex) {
     if (startIndex >= endIndex) {
       return;
     }
-    await this.stop();
     let pivot = list[startIndex];
-    let lc = startIndex;
-    let rc = endIndex;
-    this.setState({ lc, rc, pivotIndex: startIndex });
-    while (lc < rc) {
-      while (lc < rc && list[rc] >= pivot) {
-        rc--;
-        await this.stop({ rc });
+    yield this.setState({ s: startIndex });
+    let i = startIndex;
+    let j = endIndex;
+    yield this.setState({ i, j });
+
+    while (i < j) {
+      while (i < j && list[j] >= pivot) {
+        // yield this.setState({ toCompareIndex: [j, startIndex] });
+        j--;
+        yield this.setState({ j });
       }
-      while (lc < rc && list[lc] <= pivot) {
-        lc++;
-        await this.stop({ lc });
+      while (i < j && list[i] <= pivot) {
+        // yield this.setState({ toCompareIndex: [i, startIndex] });
+        i++;
+        yield this.setState({ i });
       }
-      if (lc < rc) {
-        [list[lc], list[rc]] = [list[rc], list[lc]];
-        await this.stop({ list });
+      if (i < j) {
+        yield this.setState({ toSwapIndex: [i, j] });
+        [list[i], list[j]] = [list[j], list[i]];
+        yield this.setState({ list });
       }
     }
-    [list[startIndex], list[lc]] = [list[lc], list[startIndex]];
-    await this.stop({ list });
-    await this.quickSort(list, startIndex, lc - 1);
-    await this.quickSort(list, lc + 1, endIndex);
+    yield this.setState({ toSwapIndex: [i, startIndex] });
+    [list[startIndex], list[i]] = [list[i], list[startIndex]];
+    yield this.setState({ list });
+    yield* this.quickSort(list, startIndex, i - 1);
+    yield* this.quickSort(list, i + 1, endIndex);
   }
 
-  async start() {
-    this.quickSort(this.list);
+  reset() {
+    this.setState(JSON.parse(JSON.stringify(this.initialState)));
+    this.process = this.start();
   }
 
-  renderPointer() {
-    const lc = this.state.lc;
-    const rc = this.state.rc;
-    const shell = Array(this.list.length).fill("");
-    if (lc >= 0) {
-      shell[lc] = "↑";
+  render() {
+    const items = [];
+    for (const [i, value] of Object.entries(this.state.list)) {
+      const ind = parseInt(i);
+      items.push(
+        <div
+          key={ind}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Item
+            toCompare={this.state.toCompareIndex.includes(ind)}
+            toSwap={this.state.toSwapIndex.includes(ind)}
+          >
+            {value}
+          </Item>
+          {ind === this.state.i ? (
+            <div style={{ fontSize: "1.2em" }}>i</div>
+          ) : undefined}
+          {ind === this.state.j ? (
+            <div style={{ fontSize: "1.2em" }}>j</div>
+          ) : undefined}
+          {ind === this.state.s ? (
+            <div style={{ fontSize: "1.2em" }}>🚩</div>
+          ) : undefined}
+        </div>
+      );
     }
-    if (rc >= 0) {
-      shell[rc] = "↑";
-    }
-    if (lc >= 0 && lc === rc) {
-      shell[lc] = '⇈'
-    }
-    const shell1 = Array(this.list.length).fill("");
-    const pivot = this.state.pivotIndex;
-    if (pivot >= 0) {
-      shell1[pivot] = "★";
-    }
-    return <Fragment>
-      {this.renderPointerRow(shell)}
-      {this.renderPointerRow(shell1)}
-    </Fragment>
+
+    return (
+      <>
+        <div>
+          <Button.Group className="button-group">
+            <Button onClick={(e) => this.reset()}>Reset</Button>
+            <Button
+              type="primary"
+              disabled={this.state.done}
+              onClick={(e) => this.process.next()}
+            >
+              Next
+            </Button>
+          </Button.Group>
+          {this.state.done ? (
+            <CheckCircleOutlined
+              type="success"
+              style={{ color: "#67C23A", fontSize: "1.2em", marginLeft: "1em" }}
+            />
+          ) : undefined}
+        </div>
+        <div style={{ display: "flex" }}>{items}</div>
+      </>
+    );
   }
 }
